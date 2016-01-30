ac94f9a7fa390f6dd98c812971ab9b17bce761b1
Tom Chataway <pugtura@gmail.com>
2016-01-30 12:23:29 +1100

    Created test timer to confirm Game timer functionality.

--- Unity/Spookums/Assets/Spookums/Scripts/Game.cs
+++ Unity/Spookums/Assets/Spookums/Scripts/Game.cs
@@ -3,27 +3,29 @@ using System.Collections;
 
 public class Game : MonoBehaviour
 {
-    float timer;
+    [SerializeField]private float timer = 300;
     float dt;
     bool isPaused;
     bool[] collectibles;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        timer = 300;
-        dt = 1/30;
+        dt = Time.deltaTime;
         isPaused = false;
         collectibles = new bool[5];
 
         for (int i = 0; i < collectibles.Length; i++)
             collectibles[i] = false;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-	    if (!isPaused)
+        // update delta time
+        dt = Time.deltaTime;
+
+        if (!isPaused)
         {
             timer -= dt;
 
@@ -37,7 +39,7 @@ public class Game : MonoBehaviour
                 // unlock attic
             }
         }
-	}
+    }
 
     bool EverythingCollected()
     {
@@ -48,5 +50,26 @@ public class Game : MonoBehaviour
         }
 
         return true;
+    }
+
+    public string GetTimeAsString()
+    {
+        if (timer <= 0)
+            return "00:00";
+
+        int minutes = (int)(timer / 60);
+        int seconds = (int)(timer % 60);
+
+        string result = minutes + ":";
+
+        if (seconds < 10)
+            result += "0";
+
+        return result + seconds;
+    }
+
+    public float GetTimer()
+    {
+        return timer;
     }
 }
